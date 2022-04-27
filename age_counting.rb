f81d8f8234ec8188e28ff2e8d2ae8b84224a413d
nestorbenavidez <nestorbenavidez@users.noreply.github.com>
2022-04-27 11:25:52 +0100

    Update age_counting.rb

--- age_counting.rb
+++ age_counting.rb
@@ -6,8 +6,6 @@ str_comma_sep = response.to_s.split(',')
 count = 0
 str_comma_sep.each do |str|
   split_data = str.split("=")
-  if split_data[0].strip() == "age" && split_data[1].to_i >= 50
-    count += 1
-  end
+  count += 1 if split_data[0].strip() == "age" && split_data[1].to_i >= 50  
 end
 puts count
